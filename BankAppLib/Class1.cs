ba05ef8be93fa58aab861bd262c87340eb4b1d30
Daniel <Dahlkvist>
2014-06-11 00:57:03 +0200

    Adding bankapp,
    - can parse files and generate transactions
    - can show 2 different charts with wpf toolkit
    
    Signed-off-by: Daniel <Dahlkvist>

---
+++ BankAppLib/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BankAppLib
+{
+    public class Transaction
+    {
+        public String Info { get; set; }
+        public String Date { get; set; }
+        public double Amount { get; set; }
+        public int Category { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2}\t{3}", Date, Info, Category, Amount);
+        }
+    }
+
+    public class Class1
+    {
+        public void parseFile(string path, ref List<Transaction> transactions)
+        {
+            var file = new FileInfo(path);
+            Debug.Assert(file.Exists);
+            var floatInfo = new NumberFormatInfo();
+            floatInfo.NegativeSign = "-";
+            floatInfo.CurrencyDecimalSeparator = ",";
+            floatInfo.NumberGroupSeparator = ".";
+
+            var lines = File.ReadLines(file.FullName);
+            var sb = new StringBuilder();
+            foreach (var s in lines)
+            {
+                if (String.IsNullOrEmpty(s))
+                    continue;
+                var parts = s.Split(',').ToArray();
+                var foo = parts.ToList();
+                if (parts.Length < 5 || parts.Length > 10)
+                {
+                    sb.AppendLine("Failed parsing line: " + s);
+                    continue;
+                }
+
+                if(parts[1].StartsWith("\"")) {
+                    foo[1] += " " + foo[2];
+                    foo.RemoveAt(2);
+                    parts = foo.ToArray();
+                }
+                var t = new Transaction() { Date = parts[0], Info = parts[1] };
+                var cnt = parts[0].Length + parts[1].Length + parts[2].Length + 3;
+                var line = s.Substring(cnt);
+                int amt = 0;
+                line = line.Replace("\"", "");
+                parts = line.Split(',');
+                if (parts.Length < 2 || parts.Length > 4)
+                {
+                    sb.AppendLine("Failed parsing amount parts: " + s);
+                    continue;
+                }
+
+                if (int.TryParse(parts[0], NumberStyles.AllowLeadingSign | NumberStyles.AllowThousands, floatInfo, out amt))
+                {
+                    t.Amount = amt;
+                }
+                else
+                {
+                    sb.AppendLine("Failed parsing amount: " + s);
+                    continue;
+                }
+                transactions.Add(t);
+            }
+            Console.WriteLine("Errors: \n", sb.ToString());
+            int asdf = transactions.Count;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using BankAppLib;
+
+namespace BankApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foo = new Class1();
+            var transactions = new List<Transaction>();
+            foo.parseFile("../../_assets/export.csv", ref transactions);
+            foo.parseFile("../../_assets/export2.csv", ref transactions);
+            int poo = 0;
+            poo++;
+
+            _grid.ItemsSource = transactions;
+
+            showColumnChart();
+        }
+
+        private void showColumnChart()
+        {
+            List<KeyValuePair<string, int>> MyValue = new List<KeyValuePair<string, int>>();
+            MyValue.Add(new KeyValuePair<string, int>("Mahak", 300));
+            MyValue.Add(new KeyValuePair<string, int>("Pihu", 250));
+            MyValue.Add(new KeyValuePair<string, int>("Rahul", 289));
+            MyValue.Add(new KeyValuePair<string, int>("Raj", 256));
+            MyValue.Add(new KeyValuePair<string, int>("Vikas", 140));
+            ColumnChart1.DataContext = MyValue;
+            PieChart1.DataContext = MyValue;
+        }
+    }
+}
