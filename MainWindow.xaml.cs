4eecf94290a53f6ae90c28fc05511943ee976437
Daniel <Dahlkvist>
2014-06-13 17:36:02 +0200

    possible to save and load modified values
    Signed-off-by: Daniel <Dahlkvist>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,19 +27,19 @@ namespace BankApp
 
     public class ViewTransaction
     {
-        private Transaction _transaction;
-        public string Description { get { return _transaction.Info; } }
-        public string Date { get { return _transaction.Date; } }
-        public double Amount { get { return _transaction.Amount; } }
+        public Transaction transaction { get; private set; }
+        public string Description { get { return transaction.Info; } }
+        public string Date { get { return transaction.Date; } }
+        public double Amount { get { return transaction.Amount; } }
         public TransactionCategory Category 
         { 
-            get { return (TransactionCategory)_transaction.Category; } 
-            set { _transaction.Category = (int)value; } 
+            get { return (TransactionCategory)transaction.Category; } 
+            set { transaction.Category = (int)value; } 
         }
 
         public ViewTransaction(Transaction t)
         {
-            _transaction = t;
+            transaction = t;
         }
     }
 
@@ -54,18 +54,7 @@ namespace BankApp
         {
             InitializeComponent();
             _transactions = new List<ViewTransaction>();
-
-            var foo = new BankLib(Console.Out);
-            var transactions = foo.Load();
-            foreach (var t in transactions)
-            {
-                _transactions.Add(new ViewTransaction(t));
-            }
-            //var transactions = new List<Transaction>();
-            //foo.parseFile("../../_assets/export.csv", ref transactions);
-            //foo.parseFile("../../_assets/export2.csv", ref transactions);
-            _grid.ItemsSource = _transactions;
-            showColumnChart();
+            onLoad(null, null);
         }
 
         private void showColumnChart()
@@ -85,6 +74,8 @@ namespace BankApp
             if (_grid.SelectedItems.Count > 0)
             {
                 var box = sender as ComboBox;
+                if (box.SelectedValue == null)
+                    return;
                 var category = (TransactionCategory)box.SelectedValue;
                 foreach (ViewTransaction t in _grid.SelectedItems)
                 {
@@ -92,6 +83,36 @@ namespace BankApp
                 }
                 _grid.Items.Refresh();
             }
+        }
+
+        private void onSave(object sender, ExecutedRoutedEventArgs e)
+        {
+            var transactions = new List<Transaction>();
+            foreach (var t in _transactions)
+            {
+                transactions.Add(t.transaction);
+            }
+            new BankLib(Console.Out).Save(transactions);
+        }
+
+        private void onLoad(object sender, ExecutedRoutedEventArgs e)
+        {
+            var foo = new BankLib(Console.Out);
+            var transactions = foo.Load();
+            foreach (var t in transactions)
+            {
+                _transactions.Add(new ViewTransaction(t));
+            }
+            //var transactions = new List<Transaction>();
+            //foo.parseFile("../../_assets/export.csv", ref transactions);
+            //foo.parseFile("../../_assets/export2.csv", ref transactions);
+            _grid.ItemsSource = _transactions;
+            showColumnChart();
+        }
+
+        private void CommandBinding_Executed_3(object sender, ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
